9bde7c26548371cfff68fb61330fa7a5a096deae
Your Name <josefdaly@gmail.com>
2015-06-19 16:31:58 -0700

    Reader allows font size change

--- app/assets/javascripts/views/books/read.js
+++ app/assets/javascripts/views/books/read.js
@@ -1,13 +1,18 @@
 ReadMe.Views.BookRead = Backbone.View.extend({
   initialize: function () {
     this.listenToOnce(this.model, 'sync', this.startBook)
+    this.font = 1;
+    this.fontStyle = 'serif';
   },
 
   template: JST['books/read'],
 
   events: {
     'click div#prev': 'prevPage',
-    'click div#next': 'nextPage'
+    'click div#next': 'nextPage',
+    'click .plus': 'fontUp',
+    'click .minus': 'fontDown',
+    'click .style': 'toggleFont'
   },
 
   className: 'read-page',
@@ -21,6 +26,7 @@ ReadMe.Views.BookRead = Backbone.View.extend({
   startBook: function () {
     this.Book = ePub(this.model.get('doc_url'))
     this.Book.renderTo("area")
+    debugger
   },
 
   prevPage: function () {
@@ -29,5 +35,25 @@ ReadMe.Views.BookRead = Backbone.View.extend({
 
   nextPage: function () {
     this.Book.nextPage();
+  },
+
+  fontUp: function () {
+    this.font = this.font + .1;
+    this.Book.setStyle("font-size", this.font + "em");
+  },
+
+  fontDown: function () {
+    this.font = this.font - .1;
+    this.Book.setStyle("font-size", this.font + "em");
+  },
+
+  toggleFont: function () {
+    if (this.fontStyle === 'serif') {
+      this.Book.setStyle("font-style", "sans-serif");
+      this.fontStyle = 'sans-serif';
+    } else {
+      this.Book.setStyle("font-style", 'serif');
+      this.fontStyle = 'serif';
+    }
   }
 })
