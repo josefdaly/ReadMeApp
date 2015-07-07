9e540b18b27dea8fdd39cfc658da95217d99419b
Your Name <josefdaly@gmail.com>
2015-07-06 17:26:40 -0700

    Put upload form in modal

--- app/assets/javascripts/views/navbar.js
+++ app/assets/javascripts/views/navbar.js
@@ -9,7 +9,10 @@ ReadMe.Views.NavBar = Backbone.CompositeView.extend({
     // 'click .upload-doc': 'redirectNewBook',
     'click .log-out': 'logOut',
     'click .home-page': 'redirectHomePage',
-    'click .search': 'redirectBookSearch'
+    'click .search': 'redirectBookSearch',
+    'click button.create-book': 'createBook',
+    'click button.upload-file': 'uploadFile',
+    'click button.upload-cover': 'uploadCover'
   },
 
   render: function () {
@@ -42,4 +45,45 @@ ReadMe.Views.NavBar = Backbone.CompositeView.extend({
       }
     })
   },
-})
+
+  createBook: function (event) {
+    event.preventDefault();
+    var title = $('.book-title').val();
+    var description = $('.description').val();
+    var newBook = new ReadMe.Models.Book({
+      title: title,
+      doc_url: this.url,
+      cover_url: this.cover_url,
+      description: description,
+      author_id: window.CURRENT_USER_ID
+    })
+    newBook.save({}, {
+      success: function () {
+        this.model.written_works().add(newBook);
+        Backbone.history.navigate("", { trigger: true });
+      }.bind(this)
+    });
+  },
+
+  uploadFile: function (event) {
+    event.preventDefault();
+    var that = this;
+    cloudinary.openUploadWidget(window.CLOUDINARY_SETTINGS_BOOK,
+      function (error, payload) {
+        $('.book-file-name').html(payload[0].original_filename);
+        that.url = payload[0].secure_url
+      }
+    );
+  },
+
+  uploadCover: function (event) {
+    event.preventDefault();
+    var that = this;
+    cloudinary.openUploadWidget(window.CLOUDINARY_SETTINGS_COVER,
+      function (error, payload) {
+        $('.cover-file-name').html(payload[0].original_filename);
+        that.cover_url = payload[0].secure_url
+      }
+    );
+  }
+});
