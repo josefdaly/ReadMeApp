114940e804bb5a7db5d819dcfdbd9540774e027b
Your Name <josefdaly@gmail.com>
2015-10-25 13:01:19 -0700

    Optimize obvious n+1 queries in books controller. All instances of render :index should be passed a @book that has preloaded authors and reviews.

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -20,12 +20,12 @@ module Api
     end
 
     def show
-      @book = Book.includes(:author, :subjects).find(params[:id])
+      @book = Book.includes(:author, :subjects, :reviews).find(params[:id])
       render :show
     end
 
     def index
-      @books = Book.all
+      @books = Book.includes(:author, :reviews).all
 
       render :index
     end
@@ -44,7 +44,7 @@ module Api
 
         # search by title and author name
         # still buggy
-        @books = Book.joins(:author).where(
+        @books = Book.includes(:author, :reviews).joins(:author).where(
           "LOWER(books.title) ~ ? OR LOWER(users.fname || ' ' || users.lname) ~ ?",
           params[:query].downcase,
           params[:query].downcase
@@ -57,7 +57,7 @@ module Api
     end
 
     def recent
-      @books = Book.last(4)
+      @books = Book.includes(:author, :reviews).last(4)
 
       render :index
     end
@@ -78,7 +78,7 @@ module Api
         id_arr.delete_at(rand_index)
       end
 
-      @books = Book.find(sample_id_arr.split).shuffle!
+      @books = Book.includes(:author, :reviews).find(sample_id_arr.split).shuffle!
       render :index
     end
 
